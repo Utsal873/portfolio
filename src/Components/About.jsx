94b2f514375623ee3090de9dd45083657a0b20f9
= <=>
2024-10-02 07:53:04 +0545

    project changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -57,29 +57,29 @@ const About = ({ data }) => {
 
           <div className="row">
 
-            <div className="col-lg-4 col-md-6">
+            {/* <div className="col-lg-4 col-md-6">
               <div className="count-box">
                 <i className="bi bi-emoji-smile"></i>
                 <span data-purecounter-start="0" data-purecounter-end="232" data-purecounter-duration="1" className="purecounter"></span>
                 <p>Happy Clients</p>
               </div>
-            </div>
+            </div> */}
 
-            <div className="col-lg-4 col-md-6 mt-5 mt-md-0">
+            {/* <div className="col-lg-4 col-md-6 mt-5 mt-md-0">
               <div className="count-box">
                 <i className="bi bi-journal-richtext"></i>
                 <span data-purecounter-start="0" data-purecounter-end="521" data-purecounter-duration="1" className="purecounter"></span>
                 <p>Projects</p>
               </div>
-            </div>
+            </div> */}
 
-            <div className="col-lg-4 col-md-6 mt-5 mt-lg-0">
+            {/* <div className="col-lg-4 col-md-6 mt-5 mt-lg-0">
               <div className="count-box">
                 <i className="bi bi-headset"></i>
                 <span data-purecounter-start="0" data-purecounter-end="1463" data-purecounter-duration="1" className="purecounter"></span>
                 <p>Hours Of Support</p>
               </div>
-            </div>
+            </div> */}
           </div>
 
         </div>
